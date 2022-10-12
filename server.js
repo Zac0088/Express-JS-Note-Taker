1d2def8a7460f36be94364fa8d45c85cce9ee77b
Zac Smart <zac_chaz@outlook.com>
2022-10-12 19:31:30 +1100

    delete note function

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const { join } = require("path");
+const { text } = require("body-parser");
 const app = express();
 const PORT = process.env.port || 3001;
 
@@ -35,4 +37,23 @@ app.post('/routes/note.js', (req, res)=>{
         };
         newDB.push(newNote);
     }
-    })
+    fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(newDB, null, 2), (err)=>{
+        if (err) throw err;
+        res.json(req.body);
+    });
+    });
+});
+
+appdelte('/note/id (req, res') => {
+    id = parseInt(params.id);
+    fs.readFile(join)(__dirname, '/db/db.json'), 'utf8', =>{
+        if (err) throw err;
+        const newDB =[];
+        const newNote = {
+            title: db[i].title,
+            text: db[i].text,
+            id: db[i].text
+        };
+        newDB.push(newNote);
+    }
+}
