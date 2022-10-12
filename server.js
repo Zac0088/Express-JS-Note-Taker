f6375e734e453578c086e6603fb7c454e25277f5
Zac Smart <zac_chaz@outlook.com>
2022-10-12 13:51:45 +1100

    working on pathes not sure if they are linking properly

--- server.js
+++ server.js
@@ -5,4 +5,20 @@ const app = express();
 const PORT = process.env.port || 3001;
 
 app.use(express.urlencoded({extended: true}));
-app.use(express.static)
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/public/assets/index.html'));
+});
+app.get('/public/assets/notes.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/assets/notes.html'));
+});
+app.get('/routes/notes.js', (req, res)=>{
+    fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', (err, data)=>{
+        if (err) throw err;
+        res.json(JSON.parse(data));
+    });
+
+});
+
