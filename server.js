14f811c701a7ab3d0f4fa8e9efebc3e77220a53b
Zac Smart <zac_chaz@outlook.com>
2022-10-12 19:13:51 +1100

    new note

--- server.js
+++ server.js
@@ -21,4 +21,18 @@ app.get('/routes/notes.js', (req, res)=>{
     });
 
 });
-
+app.post('/routes/note.js', (req, res)=>{
+    fs.readFile(path.join(__dirname, '/db/db,json'), 'utf8', (err,data)=> {
+        if (err) throw err;
+        const db =JSON.parse(data);
+        const newDB = [];
+        db.push(req.body);
+        for (let i = 0; 1 < dblength; i++)
+        { const newNote = {
+            title: db[i].title,
+            test: db[i].text,
+            id: i
+        };
+        newDB.push(newNote);
+    }
+    })
