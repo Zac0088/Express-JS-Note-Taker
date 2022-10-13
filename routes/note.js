be8293b0f5cae6f897697ce8ae4489539a042ff7
Zac Smart <zac_chaz@outlook.com>
2022-10-14 00:17:45 +1100

    read json file,return notes

--- routes/note.js
+++ routes/note.js
@@ -12,24 +12,48 @@ function jsonWriter(jsobject) {
         } else {
             console.log("Added file");
         }
-        
+
     });
 }
 
-router.get('/', (req, res) =>{
-    fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, jsonstring)=>{
+router.get('/', (req, res) => {
+    fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, jsonstring) => {
         if (err) {
             console.log("Error reading the filke;", err);
             return
         }
         try {
-            const notes = parse(jsobject);
-            console.log("Exisiting notes:",);
-            res.json;
+            const notes = JSON.parse(jsonstring);
+            console.log("existing notes include:", notes);
+            res.json(notes);
         }
         catch (err) {
             console.log("Failed parsing JSON:",)
         }
-        
+
     })
 });
+
+router.post('/', (req, res) => {
+    const newnote = req.body;
+    newnote.id = uuid.v4();
+
+    fs.readFile(path.join(__dirname, '../../db/db.json'), 'utf8', (err, jsonstring) => {
+        if (err) {
+            console.log("File read error:", err);
+            return;
+        }
+        try {
+            const notes = JSON.parse(jsonstring);
+            notes.push(newnote);
+            console.log("saved notes:", notes);
+            jsonWriter(notes);
+            res.json(notes);
+        }
+            catch (err) {
+                console.log("Error parsing string", err);
+            }
+
+
+        })
+})
